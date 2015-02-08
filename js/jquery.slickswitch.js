99c63eff88792582ece2b94db567ac0b361e9600
Luke Kirby <luke.kirby@mail.com>
2015-02-08 16:40:29 +0000

    Code optimisation
    Updated readme file with changed option for setting animation duration.
    
    Redone several parts of code to use native DOM methods which are quicker
    than their jQuery equivalents:
    
    Settings is now auto extended by $.extend - removed if statement
    checking if options were passed into SlickSwitch.
    
    Changed useAnimation to animationDuration as the user may want to
    customise the animation duration. e.g. 250ms will feel more snappy than
    the previous 1000ms default.
    
    On click event handler changed to use jQuery's on("click", function)
    method instead of .click so it uses less memory.
    
    Replaced various instances of self.is(":checked") with self[0].checked

--- js/jquery.slickswitch.js
+++ js/jquery.slickswitch.js
@@ -14,53 +14,67 @@
 */
 
 (function ($) {
+
+    $.fn.slickswitch = function (method) {
+        if (methods[method]) {
+        	return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist');
+        }
+    };
+	
     var methods = {
         init: function (options) {
-            var settings = {
+		
+            var settings = $.extend( {
                 cssPrefix: 'ss-',
-                useAnimation: true,
-                toggled: function () { },
-                toggledOn: function (e) { changeStatus(e); },
-                toggledOff: function (e) { changeStatus(e); }
-            };
+                animationDuration: 1000,
+                toggled: function (e) { },
+                toggledOn: function (e) { },
+                toggledOff: function (e) { }
+            }, options);
 
-            if (options) {
-                $.extend(settings, options);
-            }
-
-            this.each(function () {
+            return this.each(function () {
                 var self = $(this);
                 var div = $('<a>');
                 
+				// Check if SlickSwitch is already initialised on this element
+				if (self.hasClass(settings.cssPrefix + 'loaded')) {
+					return;
+				}
+				
                 self.bind('ss-toggle', function () {
-                    if (self.is(':checked')) {
-                        self.removeAttr('checked');
+					var elem = self[0];
+                    if (elem.checked) {
+                        elem.checked = false;
                         settings.toggledOff(self);
                     } else {
-                        self.attr('checked', 'checked');
+                        elem.checked = true;
                         settings.toggledOn(self);
                     }
-                    self.trigger('ss-update');
                     settings.toggled(self);
+                    self.trigger('ss-update');
                 });
 
                 self.bind('ss-toggleOn', function () {
-                    self.attr('checked', 'checked');
-                    self.trigger('ss-update');
+                    self[0].checked = true;
                     settings.toggledOn(self);
                     settings.toggled(self);
+                    self.trigger('ss-update');
                 });
 
                 self.bind('ss-toggleOff', function () {
-                    self.removeAttr('checked');
-                    self.trigger('ss-update');
+                    self[0].checked = false;
                     settings.toggledOff(self);
                     settings.toggled(self);
+                    self.trigger('ss-update');
                 });
 
                 self.bind('ss-update', function (o, disableAnimation) {
 
-					if ( self.is(':checked') ) {
+					if ( self[0].checked ) {
 						setTimeout(function() {
 							$('span', div).show(disableAnimation ? 0 : settings.animationDuration || 500);
 						}, 10);
@@ -69,17 +83,23 @@
 							$('span', div).hide(disableAnimation ? 0 : settings.animationDuration || 500);
 						}, 10);
                     }
+
                 });
 
-                self.after(
+				// Use native DOM method to create span element instead of using $('<span>').addClass('...')
+				var switchElem = document.createElement("span");
+					switchElem.className = settings.cssPrefix + 'on';
+				
+				self.after(	
 					div.attr('class', self.attr('class'))
-						.append($('<span>').addClass(settings.cssPrefix + 'on'))
-						//.append($('<span>').addClass(settings.cssPrefix + 'slider'))
-						.click(function () {
-						    self.trigger('ss-toggle');
-						    return false;
+						.append(switchElem)
+						.on("click", function() {
+							self.trigger('ss-toggle');
+							return false;
 						})
 				);
+				
+				self.addClass(settings.cssPrefix + 'loaded');
 
                 self.trigger('ss-update', true);
                 self.hide();
@@ -93,17 +113,6 @@
         },
         toggleOff: function () {
             $(this).trigger('ss-toggleOff');
-        }
-    };
-
-
-    $.fn.slickswitch = function (method) {
-        if (methods[method]) {
-        	return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-        } else if (typeof method === 'object' || !method) {
-            return methods.init.apply(this, arguments);
-        } else {
-            $.error('Method ' + method + ' does not exist');
         }
     };
 })(jQuery);
